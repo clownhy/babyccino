cc0abaf911d0530348b4842e6ccb00a96bfee0cc
Harvey <clownhy@gmail.com>
2025-03-25 18:05:47 +1100

    Fix bugs

---
+++ src/bootstrap.js
@@ -0,0 +1,5 @@
+// 导入jQuery
+import jQuery from 'jquery';
+window.$ = window.jQuery = jQuery;
+
+// Bootstrap通过CDN加载
--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import './assets/css/style.css';
-import '@fortawesome/fontawesome-free/css/all.css'
 
-// 确保bootstrap全局可用
-window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js');
+// Bootstrap
+import './bootstrap';
+
+// 基本依赖
+import './assets/css/style.css';
+import '@fortawesome/fontawesome-free/css/all.css';
 
 Vue.config.productionTip = false;
 
