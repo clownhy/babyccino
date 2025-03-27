aa53c5208b5b133d04345b619f39783b94edee94
Harvey <clownhy@gmail.com>
2025-03-27 12:27:08 +1100

    Update files

--- src/router/index.js
+++ src/router/index.js
@@ -33,7 +33,7 @@ const routes = [
 
 const router = new VueRouter({
   mode: 'hash',
-  base: process.env.BASE_URL,
+  base: process.env.NODE_ENV === 'production' ? '/babyccino/' : '/',
   routes,
 });
 
